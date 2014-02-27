65da099bd30993c52c9e68dd6b21e76039b2e8dc
root <root@ns397456.ip-192-95-26.net>
2014-02-27 16:01:47 -0500

    update

--- index.php
+++ index.php
@@ -4,16 +4,22 @@
     <meta charset="UTF-8">
     <title>Plugin Slider</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
 
+        <div class="copyright">
+            <p>Illustrations utilisées avec l'aimable autorisation d'<a href="http://adrien.noterdaem.be/">Adrien Noterdaem</a>,
+            créateur de <a href="http://drawthesimpsons.tumblr.com/">Simpsonized</a>.</p>
+        </div>
+
     
         <div id="slider" >
-          <img src="http://25.media.tumblr.com/a226552f4882755c8bb3d928d5339123/tumblr_mojozrDdKn1snftoqo1_1280.jpg" alt="Joffrey Baratheon" title="Joffrey Baratheon" />
-            <img src="http://24.media.tumblr.com/b7aadd3ff0c34d1202116b1b27f29aa8/tumblr_mouus4PUzJ1snftoqo1_1280.jpg" alt="Samwell Tarly" title="Samwell Tarly" />
-            <img src="http://24.media.tumblr.com/e54707bdf9162a11be96df370c18a709/tumblr_mpeou1ZWff1snftoqo1_1280.jpg" alt="Sansa Stark" title="Sansa Stark" />
-            <img src="http://24.media.tumblr.com/337f37af1bc2fb26098f50017e0fa532/tumblr_mojruwUznM1snftoqo1_1280.jpg" alt="John Snow" title="John Snow"/>
-            <img src="http://24.media.tumblr.com/29efbbda4d44d5aba695ada2f94fade5/tumblr_moji29Re3x1snftoqo1_1280.jpg" alt="Tyrion Lannister" title="Tyrion Lannister"/>
+            <img src="img/joffrey.jpg" alt="Joffrey Baratheon" title="Joffrey Baratheon" />
+            <img src="img/samwell.jpg" alt="Samwell Tarly" title="Samwell Tarly" />
+            <img src="img/sansa.jpg" alt="Sansa Stark" title="Sansa Stark" />
+            <img src="img/snow.jpg" alt="John Snow" title="John Snow"/>
+            <img src="img/tyrion.jpg" alt="Tyrion Lannister" title="Tyrion Lannister"/>
         </div>    
     
 
